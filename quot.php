76308bc0b7ecb9bc27dcfe5afa5d9e270c975a99
Scriptim <Scriptim@gmx.de>
2019-03-26 10:13:14 +0100

    Strip HTML tags

--- quot.php
+++ quot.php
@@ -1,11 +1,12 @@
 <?php
+$allowed_tags = '<b><i><u>';
 $quot_added = mysqli_real_escape_string($connection, date('Y-m-d H:i:s'));
-$quot_class = mysqli_real_escape_string($connection, $_POST['class']);
-$quot_date = mysqli_real_escape_string($connection, $_POST['date']);
-$quot_quotation = mysqli_real_escape_string($connection, str_replace(array('„', '“', '”', ',,'), '"', $_POST['quotation']));
+$quot_class = mysqli_real_escape_string($connection, strip_tags($_POST['class'], $allowed_tags));
+$quot_date = mysqli_real_escape_string($connection, strip_tags($_POST['date']));
+$quot_quotation = mysqli_real_escape_string($connection, str_replace(array('„', '“', '”', ',,'), '"', strip_tags($_POST['quotation'], $allowed_tags)));
 
 if (isset($_POST['added'])) {
-    $quot_added = mysqli_real_escape_string($connection, $_POST['added']);
+    $quot_added = mysqli_real_escape_string($connection, strip_tags($_POST['added']));
 }
 
 if (empty($quot_date)) {
